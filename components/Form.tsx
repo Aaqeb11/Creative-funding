6a98d5ca5b1a890690a91e3a1fe50a085aeda34c
Zain <zainhussaini9898@gmail.com>
2024-12-27 04:39:02 +0530

    form connected to new crm

--- components/Form.tsx
+++ components/Form.tsx
@@ -18,29 +18,31 @@ const Form = () => {
   return (
     <div className="w-full h-auto my-10">
       <iframe
-        src="https://api.leadconnectorhq.com/widget/form/aPqWS6rE8IeZphKTZWUl"
+        src="https://api.leadconnectorhq.com/widget/form/6gmovo90wFcw9aZOVgAv"
         style={{
           width: "100%",
           height: "100%",
           border: "none",
           borderRadius: "3px",
         }}
-        id="inline-aPqWS6rE8IeZphKTZWUl"
-        data-layout="{'id':'INLINE'}"
-        data-trigger-type="alwaysShow"
-        data-trigger-value=""
-        data-activation-type="alwaysActivated"
-        data-activation-value=""
-        data-deactivation-type="neverDeactivate"
-        data-deactivation-value=""
-        data-form-name="Creative funding form1"
-        data-height="602"
-        data-layout-iframe-id="inline-aPqWS6rE8IeZphKTZWUl"
-        data-form-id="aPqWS6rE8IeZphKTZWUl"
-        title="Creative funding form1"
+        id="inline-6gmovo90wFcw9aZOVgAv" 
+  data-layout="{'id':'INLINE'}"
+  data-trigger-type="alwaysShow"
+  data-trigger-value=""
+  data-activation-type="alwaysActivated"
+  data-activation-value=""
+  data-deactivation-type="neverDeactivate"
+  data-deactivation-value=""
+  data-form-name="Creative Funding Form 1"
+  data-height="465"
+  data-layout-iframe-id="inline-6gmovo90wFcw9aZOVgAv"
+  data-form-id="6gmovo90wFcw9aZOVgAv"
+  title="Creative Funding Form 1"
       ></iframe>
     </div>
   );
 };
 
 export default Form;
+
+
