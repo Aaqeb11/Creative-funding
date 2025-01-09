80d6b86f67776304002df4f59e592387cc5a67c1
Aaqeb11 <aaqebahmed@gmail.com>
2025-01-09 12:15:58 +0530

    fix: meta data image

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,21 +25,21 @@ export const metadata: Metadata = {
       "Experience hassle-free lending with Creative Funding. We offer various types of loans with an easy process, trustworthy service, and round-the-clock availability. Your reliable partner in financial solutions.",
     images: [
       {
-        url: "https://creative-funding.vercel.app/logo.png",
+        url: "/logo2.png",
         width: 1200,
         height: 630,
         alt: "Creative Funding Logo",
       },
     ],
     type: "website",
-    url: "https://creative-funding.vercel.app/logo.png",
+    url: "/logo2.png",
   },
   twitter: {
     card: "summary_large_image",
     title: "Creative Funding | Simple & Trusted Loan Solutions",
     description:
       "Looking for flexible loan options? Creative Funding offers various types of loans with an easy process, trustworthy service, and 24/7 support. Your path to reliable financial solutions starts here.",
-    images: ["/logo.png"],
+    images: ["/logo2.png"],
   },
 };
 
